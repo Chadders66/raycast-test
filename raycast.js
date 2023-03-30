61842e3db8b84d9d5c763c791324bb706a332e77
Chadders <jchad666@gmail.com>
2023-03-30 22:07:11 +0100

    Vertical line bugfix

--- raycast.js
+++ raycast.js
@@ -61,18 +61,6 @@ class Player {
     // Check which block the co-ordinates are in
     let x_block = floor(input_x/50)
     let y_block = floor(input_y/50)
-    if (x_block < 0) {
-      x_block = 0
-    }
-    if (x_block > 11) {
-      x_block = 11
-    }
-    if (y_block < 0) {
-      y_block = 0
-    }
-    if (y_block > 7) {
-      y_block = 7
-    }
     return [x_block, y_block]
   }
 
@@ -137,8 +125,8 @@ class Player {
     let dx;
     let dy;
     let da;
-    let inter_x;
-    let inter_y;
+    let intr_hx;
+    let intr_hy;
     let ray_dir;
 
     // Horizontal Lines (first contact)
@@ -148,48 +136,56 @@ class Player {
       da = 270 - this.a;
       dy = this.y - floor(this.y/50)*50;
       dx = dy * tan(da);
-      inter_x = this.x-dx;
-      inter_y = this.y-dy;
-      inter_y -= 1;
+      intr_hx = this.x-dx;
+      intr_hy = this.y-dy;
+      intr_hy -= 1;
     } else {
       // Facing down
       ray_dir = "down";
       da = 90 - this.a;
       dy = ceil(this.y/50)*50 - this.y;
       dx = dy * tan(da);
-      inter_x = this.x+dx;
-      inter_y = this.y+dy;
-      inter_y += 1;
+      intr_hx = this.x+dx;
+      intr_hy = this.y+dy;
+      intr_hy += 1;
     }
 
-    let hor_hit_b = this.blockCheck(inter_x, inter_y);
+    let hor_hit_b;
     let hor_off_x = 50 * tan(da);
     let hor_hypot = 50 / cos(abs(da));
     let hor_count = 0;
     let hor_rayxy;
     let hor_rayln;
 
-    if (0 > inter_x || inter_x > 600 || 0 > inter_y || inter_y > 400) {
+    if (0 > intr_hx || intr_hx > 600 || 0 > intr_hy || intr_hy > 400) {
       // Map limits
       hor_hit_b = 1
     } else {
+      hor_hit_b = this.blockCheck(intr_hx, intr_hy);
       hor_hit_b = grid[hor_hit_b[1]][hor_hit_b[0]];
     }
 
     // Keep adding the offset until we hit terrain/limit
     while (hor_hit_b !== 1) {
+      
       hor_count++;
-      inter_x = (ray_dir == "up") ? inter_x - hor_off_x : inter_x + hor_off_x;
-      inter_y = (ray_dir == "up") ? inter_y - 50 : inter_y + 50;
-      if (0 > inter_x || inter_x > 600 || 0 > inter_y || inter_y > 400) {
+      intr_hx = (ray_dir == "up") ? intr_hx - hor_off_x : intr_hx + hor_off_x;
+      intr_hy = (ray_dir == "up") ? intr_hy - 50 : intr_hy + 50;
+
+      if (0 > intr_hx || intr_hx > 600 || 0 > intr_hy || intr_hy > 400) {
         // Map limits
         break;
       }
-      hor_hit_b = this.blockCheck(inter_x, inter_y);
+
+      hor_hit_b = this.blockCheck(intr_hx, intr_hy);
       hor_hit_b = grid[hor_hit_b[1]][hor_hit_b[0]];
     }
-    hor_rayxy = [inter_x,inter_y];
-    hor_rayln = sqrt(dx^2 + dy^2) + (hor_count * hor_hypot);
+
+    hor_rayxy = [intr_hx,intr_hy];
+    hor_rayln = (abs(dx) / sin(abs(da))) + (hor_count * hor_hypot);
+
+    let intr_vx;
+    let intr_vy;
 
     // Vertical Lines (first contact)
     if (90 <= this.a && this.a <= 269) {
@@ -198,57 +194,59 @@ class Player {
       da = 180 - this.a;
       dx = this.x - floor(this.x/50)*50;
       dy = dx * tan(da);
-      inter_x = this.x-dx;
-      inter_y = this.y+dy;
-      inter_x -= 1;
+      intr_vx = this.x-dx;
+      intr_vy = this.y+dy;
+      intr_vx -= 1;
     } else {
       // Facing right
       ray_dir = "right"
       da = ((this.a >= 270) ? 360-this.a : 0-this.a)
       dx = ceil(this.x/50)*50 - this.x;
       dy = dx * tan(da);
-      inter_x = this.x+dx;
-      inter_y = this.y-dy;
-      inter_x += 1;
+      intr_vx = this.x+dx;
+      intr_vy = this.y-dy;
+      intr_vx += 1;
     }
 
-    let ver_hit_b = this.blockCheck(inter_x, inter_y);
+    let ver_hit_b;
     let ver_off_y = 50 * tan(da);
     let ver_hypot = 50 / cos(abs(da));
     let ver_count = 0;
     let ver_rayxy;
     let ver_rayln;
 
-    if (0 > inter_x || inter_x > 600 || 0 > inter_y || inter_y > 400) {
+    if (0 > intr_vx || intr_vx > 600 || 0 > intr_vy || intr_vy > 400) {
       // Map limits
-      ver_hit_b = 1;
+      ver_hit_b = 1
     } else {
+      ver_hit_b = this.blockCheck(intr_vx, intr_vy);
       ver_hit_b = grid[ver_hit_b[1]][ver_hit_b[0]];
     }
     
     // Keep adding the offset until we hit terrain/limit
     while (ver_hit_b !== 1) {
       ver_count++;
-      inter_y = (ray_dir == "left") ? inter_y + ver_off_y : inter_y - ver_off_y;
-      inter_x = (ray_dir == "left") ? inter_x - 50 : inter_x + 50;
-      if (0 > inter_x || inter_x > 600 || 0 > inter_y || inter_y > 400) {
+      intr_vy = (ray_dir == "left") ? intr_vy + ver_off_y : intr_vy - ver_off_y;
+      intr_vx = (ray_dir == "left") ? intr_vx - 50 : intr_vx + 50;
+
+      if (0 > intr_vx || intr_vx > 600 || 0 > intr_vy || intr_vy > 400) {
         // Map limits
         break;
       }
-      ver_hit_b = this.blockCheck(inter_x, inter_y);
+
+      ver_hit_b = this.blockCheck(intr_vx, intr_vy);
       ver_hit_b = grid[ver_hit_b[1]][ver_hit_b[0]];
     }
-    ver_rayxy = [inter_x,inter_y];
-    ver_rayln = ver_count * ver_hypot;
+    ver_rayxy = [intr_vx,intr_vy];
+    ver_rayln = (abs(dx) / cos(abs(da))) + (ver_count * ver_hypot);
 
-    if (ver_rayln < hor_rayln) {
+    if (ver_rayln <= hor_rayln) {
       strokeWeight(1);
       line(this.x,this.y,ver_rayxy[0],ver_rayxy[1]);
     } else {
       strokeWeight(1);
       line(this.x,this.y,hor_rayxy[0],hor_rayxy[1]);
     }
-
   }
 }
 
